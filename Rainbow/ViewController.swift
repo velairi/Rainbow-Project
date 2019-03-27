593d48a809ef73f97bded6d7715ec8dd1d0d37eb
Valerie Don <valdon@valeries-imac.lan>
2019-03-26 23:13:10 -0700

    Create buttons that change the background color of the view controller

--- Rainbow/ViewController.swift
+++ Rainbow/ViewController.swift
@@ -9,12 +9,48 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var redButton: UIButton!
+    @IBOutlet weak var orangeButton: UIButton!
+    @IBOutlet weak var yellowButton: UIButton!
+    @IBOutlet weak var greenButton: UIButton!
+    @IBOutlet weak var blueButton: UIButton!
+    @IBOutlet weak var purpleButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.redButton.addTarget(self, action: #selector(redButtonPressed), for: .touchUpInside)
+        self.orangeButton.addTarget(self, action: #selector(orangeButtonPressed), for: .touchUpInside)
+        self.yellowButton.addTarget(self, action: #selector(yellowButtonPressed), for: .touchUpInside)
+        self.greenButton.addTarget(self, action: #selector(greenButtonPressed), for: .touchUpInside)
+        self.blueButton.addTarget(self, action: #selector(blueButtonPressed), for: .touchUpInside)
+        self.purpleButton.addTarget(self, action: #selector(purpleButtonPressed), for: .touchUpInside)
+    }
+    
+    @objc func redButtonPressed() {
+       self.view.backgroundColor = .red
+    }
+    
+    @objc func orangeButtonPressed() {
+        self.view.backgroundColor = .orange
     }
 
-
+    @objc func yellowButtonPressed() {
+        self.view.backgroundColor = .yellow
+    }
+    
+    @objc func greenButtonPressed() {
+        self.view.backgroundColor = .green
+    }
+    
+    @objc func blueButtonPressed() {
+        self.view.backgroundColor = .blue
+    }
+    
+    @objc func purpleButtonPressed() {
+        self.view.backgroundColor = .purple
+    }
+    
 }
 
